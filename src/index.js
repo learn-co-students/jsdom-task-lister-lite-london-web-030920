230c0089ed53e08fa78db16dc6a409a6291a4003
Premti <premti@gmail.com>
2020-04-22 09:55:07 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", () => { 
+  //find form
+const wholeForm = document.querySelector('#create-task-form')
+//listen for click on submit
+wholeForm.addEventListener("submit", function(e){
+  //prevent refresh
+  e.preventDefault()
+  //find input area
+  const input = document.querySelector('#new-task-description')
+  // create new Element for input
+  const newElement = document.createElement("li")
+  // add input value to the new Element
+  newElement.innerText = input.value
+  const button = document.createElement("button")
+  button.innerHTML = "X"
+  button.addEventListener("click", function(e){
+    e.target.parentElement.remove()
+  })
+  newElement.appendChild(button)
+  //append the new element to the parent
+const tasks = document.getElementById('tasks').appendChild(newElement)
+//get rid of existing writing
+input.value = "Isak sucks"
+})
 });
